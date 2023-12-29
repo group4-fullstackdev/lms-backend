e215532ffb7db7db8d8ef83e4ea1e0966a73fbc8
Paramee <paramee.induwari@gmail.com>
2023-12-30 01:02:47 +0530

    categorizing controllers, define APIs

---
+++ lmscontrollers/lmssubmissionscontrollers.js
@@ -0,0 +1,62 @@
+const session = require('express-session');
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
+const ModSub = require('../lmsmodel/lmsmodel');
+const EnrolledMod = require('../lmsmodel/lmsmodel');
+
+// processes related to submissions
+
+// load submitted docs for staff
+
+async function getstudentsubmissions(req, res) {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' }); //change the error msg here according to prefs
+    }
+  }
+  
+  // edit submitted docs for students
+  
+  async function editsubs(req, res) {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' }); //change the error msg here according to prefs
+    }
+  }
+  
+  // delete submitted docs for students
+  
+  async function deletesubs(req, res) {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' }); //change the error msg here according to prefs
+    }
+  }
+  
+  //add submissions
+  
+  async function addsubmission(req, res) {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' }); //change the error msg here according to prefs
+    }
+  }
+  
+  //sub view for students
+  
+  async function getsubstudent(req, res) {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' }); //change the error msg here according to prefs
+    }
+  }
